360304c982efc32af725e70b96e309def6c4ec86
Smruti <smruti09052004@gmail.com>
2025-12-24 17:25:49 +0530

    added ModelArgs

--- Transformers/llama/llama-1.py
+++ Transformers/llama/llama-1.py
@@ -1,6 +1,17 @@
 import torch
 import torch.nn as nn
+from dataclasses import dataclass
 
+
+@dataclass
+class ModelArgs:
+    dim :int = 4096
+    n_layers: int = 32
+    n_heads:int = 32
+    vocab_size:int = -1
+    norm_eps:float = 1e-5
+    max_batch_size:int = 32
+    max_seq_len:int = 2048
 
 
 class SwiGLU(nn.Module):
@@ -19,7 +30,7 @@ class SwiGLU(nn.Module):
 
 
 class RMSNorm(nn.Module):
-    def __init__(self, d, eps=1e-5):
+    def __init__(self, d, eps=1e-6):
         super().__init__()
         self.eps = eps
         self.gamma = nn.Parameter(torch.ones(d))  
@@ -28,3 +39,5 @@ class RMSNorm(nn.Module):
         rms = torch.sqrt(torch.mean(x ** 2, dim=-1, keepdim=True))
         x_norm = x / (rms + self.eps)
         return x_norm * self.gamma
+
+
