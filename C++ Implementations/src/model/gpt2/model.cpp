25032114a270bc27939f5be939a0149b75a6586d
Smruti <smruti09052004@gmail.com>
2026-01-02 23:25:04 +0530

    added model config

--- C++ Implementations/src/model/gpt2/model.cpp
+++ C++ Implementations/src/model/gpt2/model.cpp
@@ -1,5 +1,17 @@
-#include "../common/Norms/layernorm.h"
+#include "../../common/Norms/layernorm.h"
 #include<iostream>
+
+struct GPT2Config
+{
+    int max_seq_len; // max sequence length, 1024
+    int vocab_size; // vocab size, 50257
+    int padded_vocab_size; // padded to
+    int num_layers; // number of layers 12
+    int num_heads; // number of heads in attention 12
+    int channels; // number of channels 768
+};
+
+
 
 int main() {
     // int C = 768;  
@@ -12,6 +24,8 @@ int main() {
     // }
     
     // LayerNorm ln(weight, bias, C);
-    
+    GPT2Config config;
+    config.channels = 768;  
+    std::cout<<config.channels<<std::endl;
     return 0;
 }
