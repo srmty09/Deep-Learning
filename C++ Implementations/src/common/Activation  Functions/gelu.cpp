247fa9e7a4cf261001109df309d30ba900b6d466
Smruti <smruti09052004@gmail.com>
2026-01-02 23:02:21 +0530

    added Gelu activation forward and backward.

---
+++ C++ Implementations/src/common/Activation  Functions/gelu.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+
+#define GELU_SCALING_FACTOR sqrtf(2.0f/M_PI)
+void gelu_forward(float* out, float* inp, int N){
+    for(int i = 0; i < N; i++){
+        float x = inp[i];
+        float cube = 0.044715f*x*x*x;
+        out[i] = 0.5f * x * (1.0f + tanhf(GELU_SCALING_FACTOR * (x+cube)));
+    }
+}
+void gelu_backward(float* dinp, float* inp,float* dout, int N){
+    for(int i = 0; i<N;i++){
+        float x = inp[i];
+        float cube = 0.044715f*x*x*x;
+        float tanh_arg = GELU_SCALING_FACTOR * (x+cube);
+        float tanh_out = tanhf(tanh_arg);
+        float coshf_out = coshf(tanh_arg);
+        float sech_out = 1.0f / (coshf_out*coshf_out);
+        float _grad = 0.5f * (1.0f + tanh_out) + x * 0.5f * sech_out * GELU_SCALING_FACTOR * (1.0f + 3.0f * 0.044715f * x * x);
+        dinp[i] += _grad * dout[i];
+    }
+}
---
+++ C++ Implementations/src/common/Activation  Functions/gelu.h
@@ -0,0 +1,23 @@
+#include <math.h>
+
+#ifndef GELU_H
+#define GELU_H
+
+void gelu_forward(float* out, float* inp, int N);
+void gelu_backward(float* dinp, float* inp,float* dout, int N);
+
+
+class Gelu {
+
+public:
+    void forward(float* out, float* inp, int N){
+        gelu_forward(out,inp,N);
+    }
+    void backward(float* dinp,float* inp, float* dout, int N){
+        gelu_backward(dinp,inp,dout,N);
+    }
+
+};
+
+
+#endif GELU_H
