233a7608ca140a28df972151d1d4685ec29f958e
Smruti <smruti09052004@gmail.com>
2026-01-02 18:21:26 +0530

    model config for gpt2

---
+++ C++ Implementations/GPT-2/src/gpt2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<math.h>
+
+
+using ll = long long;
+
+
+typedef struct{
+    int seq_len = 1024; // seq_len
+    ll vocab_size =  50257; // vocab size
+    int n_layer = 12; // number of layers
+    int n_head = 12; // number of head
+    int n_embd = 768; // embedding dimension
+}GPTconfig;
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+
+    return 0;
+}
