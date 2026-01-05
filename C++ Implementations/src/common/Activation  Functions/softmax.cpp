394af6b639a28574348f80acbc4cbba5f8583d7d
Smruti <smruti09052004@gmail.com>
2026-01-05 20:03:17 +0530

    added backward for softmax

--- C++ Implementations/src/common/Activation  Functions/softmax.cpp
+++ C++ Implementations/src/common/Activation  Functions/softmax.cpp
@@ -30,3 +30,36 @@ void softmax_forward(float* probs, float* logits,int B,
         }
     }
 }
+
+
+// dprobs: gradient from next layer (same shape as probs)
+// probs: output of softmax forward
+// dlogits: output gradient (same shape as logits)
+void softmax_backward(float* dlogits, const float* dprobs, const float* probs,
+                      int B, int T, int V, int Vp) {
+
+    for (int b = 0; b < B; b++) {
+        for (int t = 0; t < T; t++) {
+
+            float* dlogits_bt = dlogits + b*(T*Vp) + t*Vp;
+            const float* dprobs_bt = dprobs + b*(T*Vp) + t*Vp;
+            const float* probs_bt  = probs  + b*(T*Vp) + t*Vp;
+
+            // dot = sum_j (dprobs_j * probs_j)
+            float dot = 0.0f;
+            for (int i = 0; i < V; i++) {
+                dot += dprobs_bt[i] * probs_bt[i];
+            }
+
+            // dlogits_i = probs_i * (dprobs_i - dot)
+            for (int i = 0; i < V; i++) {
+                dlogits_bt[i] = probs_bt[i] * (dprobs_bt[i] - dot);
+            }
+
+            // padded part
+            for (int i = V; i < Vp; i++) {
+                dlogits_bt[i] = 0.0f;
+            }
+        }
+    }
+}
--- C++ Implementations/src/common/Activation  Functions/softmax.h
+++ C++ Implementations/src/common/Activation  Functions/softmax.h
@@ -3,6 +3,8 @@
 
 void softmax_forward(float* probs, float* logits,int B,
                 int T, int V, int Vp);
+void softmax_backward(float* dlogits, const float* dprobs, const float* probs,
+                      int B, int T, int V, int Vp);              
 
 class softmax{
 private:
@@ -11,6 +13,10 @@ public:
                 int T, int V, int Vp){
         softmax_forward(probs, logits, B,
                 T, V, Vp);
+    }
+    void backward(float* dlogits, const float* dprobs, const float* probs,
+                      int B, int T, int V, int Vp){
+        softmax_backward(dlogits,dprobs,probs,B,T,V,Vp);
     }
     
 };
