9df58d209cceb37c83b55f5d0684f40921d658c4
Smruti <smruti09052004@gmail.com>
2026-01-06 10:17:08 +0530

    outline of the whole paligemma conditonal generation

--- Vision Language Model/modeling_gemma.py
+++ Vision Language Model/modeling_gemma.py
@@ -17,3 +17,29 @@ class PaliGemmaForConditionalGeneration(nn.Module):
         self.language_model = language_model
 
         self.pad_token_id = self.config.pad_token_id if self.config.pad_token_id is not None else -1
+
+    def tie_weights(self):
+        return self.language_model.tie_weights()
+    
+    def forward(self,
+                input_ids,
+                pixel_values,
+                attention_mask,
+                kv_cache):
+        assert torch.all(attention_mask==1)
+        input_embeds = self.language_model.get_input_embeddings()(input_ids)
+
+        image_features = self.vision_tower(pixel_values.to(input_embeds.dtype))
+
+        image_features = self.multi_model_projector(image_features)
+
+        input_embeds,attention_mask,position_ids = self._merge_input_ids_with_image_feature(image_features,input_embeds,input_ids,attention_mask,kv_cache)
+
+        outputs = self.language_model(
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            input_embeds=input_embeds,
+            kv_cache=kv_cache
+        )
+
+        return outputs
