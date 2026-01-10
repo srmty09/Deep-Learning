6c62b775911ff64345c6838f2f3787cc5d882540
Smruti <smruti09052004@gmail.com>
2026-01-11 00:03:01 +0530

    BertOutput for the FFN downscaling and norm

--- Distilation/Distilled Bert/model.py
+++ Distilation/Distilled Bert/model.py
@@ -145,3 +145,19 @@ class BertIntermediate(nn.Module):
         hidden_states = self.intermediate_act_fn(hidden_states)
         return hidden_states
     
+class BertOutput(nn.Module):
+    # second FFN layer + LayerNorm
+    def __init__(self, config: DistilledBertConfig):
+        super().__init__()
+        self.cfg = config
+        self.dense = nn.Linear(config.intermediate_size, config.hidden_size)
+        self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
+    
+    def forward(self, hidden_states, input_tensor):
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+        
+        return hidden_states
