e73e69ab8d2b1429a2b825212b885157c9f90638
Smruti <smruti09052004@gmail.com>
2026-01-13 21:35:46 +0530

    added MHA, with support of cross attention.

--- BART/model.py
+++ BART/model.py
@@ -22,6 +22,41 @@ class AddNorm(nn.Module):
     def forward(self, x, residual):
         return self.ln(residual + self.dropout(x))
 
+class BartMHA(nn.Module):
+    def __init__(self, config: BartConfig, causal_mask: bool = False):
+        super().__init__()
+        self.config = config
+        self.wq = nn.Linear(config.d_model, config.d_model)
+        self.wk = nn.Linear(config.d_model, config.d_model)
+        self.wv = nn.Linear(config.d_model, config.d_model)
+        self.cproj = nn.Linear(config.d_model, config.d_model)
+        self.n_head = config.n_head
+        self.causal_mask = causal_mask
+        
+    def forward(self, tokens, enc_hidden_states=None):
+        B, T, C = tokens.shape
+        
+        if enc_hidden_states is None:  
+            q = self.wq(tokens)
+            k = self.wk(tokens)
+            v = self.wv(tokens)
+            T_kv = T
+        else:
+            k = self.wk(enc_hidden_states)
+            v = self.wv(enc_hidden_states)
+            q = self.wq(tokens)
+            T_kv = enc_hidden_states.shape[1] 
+        
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        k = k.view(B, T_kv, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T_kv, self.n_head, C // self.n_head).transpose(1, 2)
+
+        out = F.scaled_dot_product_attention(q, k, v, is_causal=self.causal_mask)
+
+        out = out.transpose(1, 2).contiguous().view(B, T, C)
+        out = self.cproj(out)
+        return out
+
 class BartEncoderLayer(nn.Module):
     def __init__(self, config: BartConfig):
         super().__init__()
@@ -41,7 +76,7 @@ class BartEncoderLayer(nn.Module):
 class BartDecoderLayer(nn.Module):
     def __init__(self, config: BartConfig):
         super().__init__()
-        self.self_attn = BartMHA(config)
+        self.self_attn = BartMHA(config,causal_mask=True)
         self.cross_attn = BartMHA(config) 
         self.ffn = BartFFN(config)
         self.add_norm1 = AddNorm(config)
